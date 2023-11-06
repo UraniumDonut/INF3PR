bab20d0daf6cd5c4775e77d84b8c1674cd20178e
Basti <sepwitzel553@gmail.com>
2023-11-06 13:20:23 +0100

    Open läuft

--- praktikum03/src/praktikum03/Controller/Commands/OpenCommand.java
+++ praktikum03/src/praktikum03/Controller/Commands/OpenCommand.java
@@ -4,6 +4,12 @@
  */
 package praktikum03.Controller.Commands;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static javax.swing.JFileChooser.APPROVE_OPTION;
+import static javax.swing.JFileChooser.ERROR_OPTION;
 import praktikum03.Model.AdressverwaltungModel;
 import praktikum03.View.Fenster;
 
@@ -20,8 +26,21 @@ public class OpenCommand implements CommandInterface{
         model = m;
     }
     @Override
-    public void execute() {    
-        view.getjFileChooser().showOpenDialog(view);
+    public void execute() {
+        int ret = view.getjFileChooser().showOpenDialog(view);
+        if (ret == ERROR_OPTION){
+            //throw new Exception("Open failed.");
+        }
+        else if (ret == APPROVE_OPTION){
+            File file = view.getjFileChooser().getSelectedFile();
+            try {
+                model.datenLesen(file);
+            } catch (IOException ex) {
+                Logger.getLogger(OpenCommand.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(OpenCommand.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }           
     }
 
     @Override
