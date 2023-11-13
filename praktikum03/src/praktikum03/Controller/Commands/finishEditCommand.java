9709fdc9ba8692ff4bec91c64a1fc29856ca0956
Basti <sepwitzel553@gmail.com>
2023-11-13 12:11:24 +0100

    aalöfdjlöfjasöfj

--- praktikum03/src/praktikum03/Controller/Commands/finishEditCommand.java
+++ praktikum03/src/praktikum03/Controller/Commands/finishEditCommand.java
@@ -12,55 +12,45 @@ import praktikum03.View.Fenster;
 
 /**
  * Speichert die Änderungen in der Tabelle bei Drücken der Enter-Taste
+ *
  * @author Leon
  */
-public class finishEditCommand implements CommandInterface
-{
+public class finishEditCommand implements CommandInterface {
 
-  Fenster view;
-  AdressverwaltungModel model;
-  Preferences pref;
+    Fenster view;
+    AdressverwaltungModel model;
+    Preferences pref;
 
-  public finishEditCommand(Fenster frm, AdressverwaltungModel m)
-  {
-    view = frm;
-    model = m;
-  }
+    public finishEditCommand(Fenster frm, AdressverwaltungModel m) {
+        view = frm;
+        model = m;
+    }
 
-  /**
-   * Speichert die Änderungen in der Tabelle bei Drücken der Enter-Taste
-   */
-  @Override
-  public void execute()
-  {
-    JTable table = view.getjTable1();
+    /**
+     * Speichert die Änderungen in der Tabelle bei Drücken der Enter-Taste
+     */
+    @Override
+    public void execute() {
+        JTable table = view.getjTable1();
         int rows = model.getRowCount();
         int col = model.getColumnCount();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < col; j++) {
-                if (!table.getValueAt(i, j).equals(model.getValueAt(i, j))){
-                    model.putRowUndoStack(i);
-                }
-            }
-        }
-    model.table2model(view);
-    model.updateTable(view);
-  }
+        model.putRowUndoStack(view.getjTable1().getSelectedRow());
+        model.table2model(view);
+        model.updateTable(view);
+    }
 
-  @Override
-  public void undo()
-  {
-    model.table2model(view);
-    RowData rd = new RowData();
-    rd = model.getRowUndoStack();
-    model.deleteRowData(rd.idx);
-    model.insertRowData(rd.idx, rd.str);
-    model.updateTable(view);              
-  }
+    @Override
+    public void undo() {
+        model.table2model(view);
+        RowData rd = new RowData();
+        rd = model.getRowUndoStack();
+        model.deleteRowData(rd.idx);
+        model.insertRowData(rd.idx, rd.str);
+        model.updateTable(view);
+    }
 
-  @Override
-  public boolean isUndoable()
-  {
-    return true;
-  }
+    @Override
+    public boolean isUndoable() {
+        return true;
+    }
 }
