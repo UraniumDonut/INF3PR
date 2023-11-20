5cb4cdd903403e69b3f14f93bd65a95d49057768
Leon Bartle <leon@bartle.eu>
2023-11-20 12:02:11 +0100

    Setup Project

---
+++ praktikum05/src/praktikum05/Model/ZahlenGenerator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package praktikum05.Model;
+
+/**
+ *
+ * @author Leon
+ */
+public class ZahlenGenerator 
+{
+  public ZahlenGenerator()
+  {
+  
+  }
+}
