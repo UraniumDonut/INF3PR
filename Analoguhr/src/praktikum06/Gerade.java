20164de7d1ccdf4816e6ec45c9bfc63891c7b9cb
Leon Bartle <leon@bartle.eu>
2023-11-27 12:08:42 +0100

    Update Gerade.java

--- Analoguhr/src/praktikum06/Gerade.java
+++ Analoguhr/src/praktikum06/Gerade.java
@@ -4,12 +4,62 @@
  */
 package praktikum06;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Line2D;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import javax.swing.JComponent;
 
 /**
  *
  * @author basti
  */
-public class Gerade extends JComponent{
+public class Gerade extends JComponent implements Runnable{
+  private static final float DICKE = 5f; //Stiftdicke
+  private BasicStroke stift;
+
+  private Line2D.Float gerade;
+
+  private ExecutorService eService;
+  private Future task;
+
+  public Gerade(){
+    gerade = new Line2D.Float();
+    stift = new BasicStroke(DICKE);
+    eService = Executors.newSingleThreadExecutor();
+    task = null;
+  }
+
+  public void start(){
+    if (task == null){
+      task = eService.submit(this);
+    }
+  }
+  
+  @Override
+  public void paintComponent(Graphics g){
+    super.paintComponent(g); // Das braucht man vor allem bei alten Systemen
+		Graphics2D g2 = (Graphics2D)g; // Cast von Graphics auf Graphics2d
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     
+    float x = this.getWidth()/2;
+    float y = this.getHeight()/2;
+    
+    gerade.setLine(x,y,x+10,y+10);
+    
+    g2.setStroke(stift);
+		g2.setPaint(Color.RED);
+		g2.draw(gerade);
+  }
+
+  @Override
+  public void run(){
+
+  } 
 }
+
