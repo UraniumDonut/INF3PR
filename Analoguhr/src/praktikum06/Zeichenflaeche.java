c4ef56cff10dee62759d9318947da08ead8e1d3a
Basti <sepwitzel553@gmail.com>
2023-11-27 11:44:30 +0100

    Projekt06 für Analoguhr angelegt

---
+++ Analoguhr/src/praktikum06/Zeichenflaeche.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum06;
+
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author basti
+ */
+public class Zeichenflaeche extends JComponent{
+    
+}
