d236c00cd5643682322cb5f18b8a8fa5a72cd038
Leon Bartle <leon@bartle.eu>
2023-12-11 12:25:02 +0100

    Update GraphicViewer.java

--- praktikum07/src/praktikum07/view/GraphicViewer.java
+++ praktikum07/src/praktikum07/view/GraphicViewer.java
@@ -27,36 +27,52 @@ import praktikum07.Start;
  */
 public class GraphicViewer extends JComponent implements Printable
 {
+
   private GraphicModel model;
   private Line2D.Float line;
-  public GraphicViewer(){
+
+  public GraphicViewer()
+  {
   }
-  public void initView(GraphicModel model){
+
+  public void initView(GraphicModel model)
+  {
     this.model = model;
     this.line = new Line2D.Float();
   }
-  
+
   @Override
-  public void paintComponent(Graphics g){
-    if (model == null){
+  public void paintComponent(Graphics g)
+  {
+    if (model == null)
+    {
       return;
     }
     super.paintComponent(g);
-    Graphics2D g2 = (Graphics2D)g;
-    model.getShapes().forEach(s -> {
+    Graphics2D g2 = (Graphics2D) g;
+    model.getShapes().forEach(s ->
+    {
       List<Point> points = s.getPoints();
-      for (int i = 0; i < points.size(); i++){
-      if (i == 0){
-        //do nothing
+      Point pointBefore = null;
+      Point pointNow;
+      for (int i = 0; i < points.size(); i++)
+      {
+        if (i == 0)
+        {
+          //do nothing
+          pointBefore = points.get(i);
+        }
+        else
+        {
+          pointNow = points.get(i);
+          line = new Line2D.Float(pointBefore, pointNow);
+          g2.draw(line);
+          pointBefore = pointNow;
+        }
       }
-      else{
-        line = new Line2D.Float(points.get(i-1), points.get(i));
-        g2.draw(line);
-      }
-    }
     });
   }
-  
+
   public void doPrint()
   {
     HashPrintRequestAttributeSet printSet = new HashPrintRequestAttributeSet();
@@ -84,7 +100,7 @@ public class GraphicViewer extends JComponent implements Printable
     if (pageIndex == 0)
     {
       gp2.translate(pf.getImageableX(), pf.getImageableX());
-      gp2.scale(pf.getImageableWidth()/pf.getWidth(), pf.getImageableHeight()/pf.getWidth());
+      gp2.scale(pf.getImageableWidth() / pf.getWidth(), pf.getImageableHeight() / pf.getWidth());
       super.print(gp2);
       return Printable.PAGE_EXISTS;
     }
