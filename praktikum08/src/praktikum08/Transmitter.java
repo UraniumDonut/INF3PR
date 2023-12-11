fc7cc57fbbdd53c7cb85c352c3afecc9760eebc1
Leon Bartle <leon@bartle.eu>
2023-12-11 12:59:49 +0100

    IsServer

--- praktikum08/src/praktikum08/Transmitter.java
+++ praktikum08/src/praktikum08/Transmitter.java
@@ -4,10 +4,51 @@
  */
 package praktikum08;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author basti
  */
-public class Transmitter {
-    
+public class Transmitter
+{
+
+  private static final int PORT = 35400;
+
+  public Transmitter()
+  {
+  }
+
+  public void initTransmitter(boolean isServer, String IP) throws IOException
+  {
+    Socket s = null;
+    if (isServer)
+    {
+      ServerSocket sSocket = new ServerSocket(PORT);
+      s = sSocket.accept(); //Das Blockiert und muss im Thread sein bei GUI Applikationen
+    }
+    else
+    {
+      s = new Socket(IP, PORT); //Das Blockiert und muss im Thread sein bei GUI Applikationen
+    }
+
+    InputStream ins = s.getInputStream();
+    OutputStream os = s.getOutputStream();
+
+    InputStreamReader isr = new InputStreamReader(ins, "UTF-8");
+    OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8"); //Das sind jetzt Strings
+
+    BufferedReader in = new BufferedReader(isr);
+    //BufferedWriter out = new BufferedWriter(osw); 
+    PrintWriter out = new PrintWriter(osr); //ist cooler, weil wir hier die println und printf funktionen haben
+  }
+
 }
