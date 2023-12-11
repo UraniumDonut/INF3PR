b6ae112440dec6cde331bafe145de7aa0a23d40e
SebMeyer-afk <117305918+SebMeyer-afk@users.noreply.github.com>
2023-12-11 12:24:46 +0100

    afdasfsaf

--- praktikum08/src/praktikum08/controller/CommandConnect.java
+++ praktikum08/src/praktikum08/controller/CommandConnect.java
@@ -5,43 +5,23 @@
 package praktikum08.controller;
 
 import java.util.concurrent.Flow;
-import java.util.concurrent.Flow.Subscriber;
-import java.util.concurrent.Flow.Subscription;
+import praktikum08.ChatView;
 import praktikum08.Transmitter;
 
 /**
  *
  * @author basti
  */
-public class CommandConnect implements Subscriber{
+public class CommandConnect{
     ChatView view;
     Transmitter model;
-    Subscription sub;
+    Flow.Subscription sub;
     
     public CommandConnect(ChatView view, Transmitter model){
         this.view = view;
         this.model = model;
     }
-
-    @Override
-    public void onSubscribe(Flow.Subscription subscription) {
-        sub = subscription;
-        sub.request(1);
-    }
-
-    @Override
-    public void onNext(Object item) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void onError(Throwable throwable) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void onComplete() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    
+    public connect()
     
 }
