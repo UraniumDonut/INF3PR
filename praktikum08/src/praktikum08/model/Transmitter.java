8f013641d9745971120fc5f27688f767b76f0e5b
Leon Bartle <leon@bartle.eu>
2023-12-18 12:39:46 +0100

    Transmitter transmitting

--- praktikum08/src/praktikum08/model/Transmitter.java
+++ praktikum08/src/praktikum08/model/Transmitter.java
@@ -4,18 +4,20 @@
  */
 package praktikum08.model;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import static java.lang.System.out;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.SubmissionPublisher;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import praktikum08.Logger.GluecksLogger;
 
@@ -23,19 +25,20 @@ import praktikum08.Logger.GluecksLogger;
  *
  * @author basti
  */
-public class Transmitter  implements Runnable
+public class Transmitter implements Runnable
 {
+
   private static Logger lg = GluecksLogger.getLogger();
 
   private static final int PORT = 8080;
-  private BufferedReader in;
-  private ObjectOutputStream out;
-  private SubmissionPublisher<String> pub;
+  private ObjectInputStream isr;
+  private ObjectOutputStream osw;
+  private SubmissionPublisher<Object> pub;
   private Thread trd;
 
   public Transmitter()
   {
-      pub = new SubmissionPublisher<String>();
+    pub = new SubmissionPublisher<Object>();
   }
 
   public void connect(boolean isServer, String IP) throws IOException
@@ -54,42 +57,56 @@ public class Transmitter  implements Runnable
     InputStream ins = s.getInputStream();
     OutputStream os = s.getOutputStream();
 
-    InputStreamReader isr = new InputStreamReader(ins, "UTF-8");
-    OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8"); //Das sind jetzt Strings
+    BufferedInputStream bis = new BufferedInputStream(ins);
+    BufferedOutputStream bos = new BufferedOutputStream(os);
 
-    in = new BufferedReader(isr);
-    BufferedWriter out = new BufferedWriter(osw); 
+    isr = new ObjectInputStream(bis);
+    osw = new ObjectOutputStream(bos);
+
     //out = new PrintWriter(osw); //ist cooler, weil wir hier die println und printf funktionen haben
-    
-    if (trd == null){
-        trd = new Thread(this);
-        trd.start();
+    if (trd == null)
+    {
+      trd = new Thread(this);
+      trd.start();
     }
   }
 
-    @Override
-    public void run() {
-      while(true){
-        try {
-            String str = in.readLine();
-            if(str != null){
-                pub.submit(str);
-            }
-            trd.sleep(100);
-        } catch (IOException ex) {
-            lg.severe(ex.getLocalizedMessage());
-        } catch (InterruptedException ex) {
-              lg.severe(ex.getLocalizedMessage());
-          }
+  @Override
+  public void run()
+  {
+    while (true)
+    {
+      try
+      {
+        Object daten = isr.readObject();
+        if (daten instanceof ArrayList list)
+        {
+          pub.submit(daten);
+        }
+        trd.sleep(100);
+      }
+      catch (Exception ex)
+      {
+        lg.severe(ex.getLocalizedMessage());
       }
     }
-    
-    public void addSubscriber(Subscriber<String> subscriber){
-        pub.subscribe(subscriber);
+  }
+
+  public void addSubscriber(Subscriber<Object> subscriber)
+  {
+    pub.subscribe(subscriber);
+  }
+
+  public void send(Object obj)
+  {
+    try
+    {
+      osw.writeObject(obj);
+      out.flush();
     }
-    
-    public void send(String msg){
-        out.println(msg);
-        out.flush();
+    catch (Exception ex)
+    {
+      lg.severe(ex.getLocalizedMessage());
     }
+  }
 }
