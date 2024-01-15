87841a3bec5660b3aea2365a1d0446be72e7233e
Leon Bartle <leon@bartle.eu>
2024-01-15 12:06:09 +0100

    Deleting the leftover files from secondary

--- praktikum10/src/main/java/praktikum10/FxController.java
+++ praktikum10/src/main/java/praktikum10/FxController.java
@@ -3,7 +3,7 @@ package praktikum10;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class FxController {
 
     @FXML
     private void switchToSecondary() throws IOException {
